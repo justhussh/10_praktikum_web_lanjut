5dfc8c3b00f2948b685a83b58eb6d2b626a478b3
justhussh <azzam17102003@gmail.com>
2023-05-09 20:45:08 +0700

    Pertemuan 10 (Praktikum 2)

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ArticleController extends Controller
 {
@@ -66,7 +67,8 @@ class ArticleController extends Controller
      */
     public function edit(Article $article)
     {
-        //
+        $article = Article::find($id);
+        return view('articles.edit', ['article' => $article]);
     }
 
     /**
@@ -78,7 +80,19 @@ class ArticleController extends Controller
      */
     public function update(Request $request, Article $article)
     {
-        //
+        $article = Article::find($id);
+
+        $article->title = $request->title;
+        $article->content = $request->content;
+
+        if ($article->featured_image && file_exists(\storage_path('app/public/' .$article->featured_image))) {
+            \Storage::delete('public/' .$article->featured_image);
+        }
+        $image_name = $request->file('image')->store('images', 'public');
+        $article->featured_image - $image_name;
+
+        $article->save();
+        return 'Artikel berhasil diubah';
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 
-Route::resource('articles', ArticleController::class);
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,5 +21,5 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
+Route::resource('articles', ArticleController::class);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
